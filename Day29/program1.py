d9e574aaab057d05a9d6184fba7d9e5dae5b96a8
Harshit Beniwal <typicalshadow88@gmail.com>
2026-06-30 11:18:07 +0530

    Day29 uploaded.

---
+++ Day29/program1.py
@@ -0,0 +1,33 @@
+while True:
+
+    print("\n1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 5:
+        break
+
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    if choice == 1:
+        print("Answer =", a + b)
+
+    elif choice == 2:
+        print("Answer =", a - b)
+
+    elif choice == 3:
+        print("Answer =", a * b)
+
+    elif choice == 4:
+        if b != 0:
+            print("Answer =", a / b)
+        else:
+            print("Cannot divide by zero")
+
+    else:
+        print("Invalid Choice")
